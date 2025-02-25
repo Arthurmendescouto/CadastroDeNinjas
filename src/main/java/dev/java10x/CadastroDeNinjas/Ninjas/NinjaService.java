1123f5e8ccb8d065ef65e5e2d2944cdafe242411
Arthur <arthurmendescouto16@gmail.com>
2025-02-24 22:45:09 -0300

    feat: Inclui a rota de cadastro de ninjas usando metodo POST, tambem inicializa o Postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -22,4 +22,8 @@ public class NinjaService {
         return ninjaModel.orElse(null);
     }
 
+    public NinjaModel criarNinja(NinjaModel ninja){
+        return ninjaRepository.save(ninja);
+    }
+
 }
